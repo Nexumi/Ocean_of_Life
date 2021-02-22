55b8db7d7dafabd796392b8c0dd875801a30348b
Nexumi <48866805+Nexumi@users.noreply.github.com>
2021-02-22 14:23:40 -0800

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', view);
+window.addEventListener('load', offset);
+window.addEventListener('resize', offset);
+document.getElementById("credits").addEventListener('click', soon);
+var OoL = document.getElementById("OoL");
+OoL.volume = 0.5;
+
+function offset() {
+	if (window.innerWidth < window.innerHeight) {
+		OoL.style.width = "100%";
+		OoL.style.left = "0%";
+	} else {
+		OoL.style.width = "50%";
+		OoL.style.left = "25%";
+	}
+	OoL.style.height = OoL.offsetWidth / 16 * 9 + "px";
+	OoL.style.marginTop = -(OoL.offsetHeight/2) - 60 + "px";
+}
+
+function view() {
+	OoL.style.display = "block";
+}
+
+function soon() {
+	alert("=Under Construction=\nPlease check back in a few days.");
+}
